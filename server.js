c08959f8ced92758cae8d2ebccd7ddae0a03ba1e
Bridget <bridget.wilson2@griffithuni.edu.au>
2022-08-11 19:01:33 +1000

    task 3 to 4

--- server.js
+++ server.js
@@ -1,17 +1,72 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 
 app.use(express.static('./www'));
 
-let server = http.listen(3000, function () {
-    let host = server.address().address;
-    let port = server.address().port;
-    console.log('My First Nodejs Server!');
-    console.log('Server listening on: ' + host + 'port:' + port);
-}); // server
-
-
-app.get('/test', function (req, res) {
-    res.sendFile(__dirname + '/www/test.html');
+app.listen(3000, 'localhost', function(){
+    var d = new Date();
+    var n = d.getHours();
+    var m = d.getMinutes();
+    console.log('Server has started listening at: ' + n + ":" + m); 
 });
+
+
+// var http = require('http').Server(app);
+// app.use(express.static('./www'));
+
+// let server = http.listen(3000, function () {
+//     let host = server.address().address;
+//     let port = server.address().port;
+//     console.log('My First Nodejs Server!');
+//     console.log('Server listening on: ' + host + 'port:' + port);
+// }); // server
+
+
+// ================================
+// Routes 
+// ================================
+
+// Default Route
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/www/index.html');
+});
+
+// Account Route
+app.get('/account', function (req, res) {
+    res.sendFile(__dirname + '/www/account.html');
+});
+
+// Credential verification
+app.post('/api/login', function(req, res){
+
+    let users = [
+        {'userEmail' : 'b'      ,'userPass' : '1'}, 
+        {'userEmail' : 'Kira@desunoto.jp'       ,'userPass' : 'Shinigami'}, 
+        {'userEmail' : 'monkey@business.club'   ,'userPass' : 'monke'}
+    ];
+
+    if (!req.body){
+        return res.sendStatus(400)
+    }
+        var user = {};
+        user.userEmail = req.body.userEmail;
+        user.userPass  = req.body.userPass;
+        user.valid = false;
+
+    // loop through the users list and check the entry to the values in the users object
+    for (let i = 0; i < users.length; i++){
+        
+        if (req.body.userEmail == users[i].userEmail && req.body.userPass == users[i].userPass){
+            user.valid = true;
+        }
+
+        // else{
+        //     user.valid = false; // might not need this as its the default?
+        // }
+    }   
+        res.send(user);
+});
+
---
+++ www/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    console.log('jQuery Ready');
+    
+    $("#loginForm").submit(function(event) {
+        event.preventDefault();
+        ajaxPost();
+    });
+
+    function ajaxPost(){
+        var formData ={
+            userEmail: $("#userEmail").val(), 
+            userPass:  $("#userPass").val()
+        }
+        $.ajax({
+            type: "POST", contentType: "application/json",
+            url: window.location + "api/login", 
+            data: JSON.stringify(formData), 
+            dataType: 'json',
+            success : function(user) {
+
+                if (user.valid == true) {
+                    $("errormsg").addClass("hideMessage");
+                    $("#loginForm").removeClass("fail");
+                    $("#loginForm").addClass("success");
+
+                    
+                    
+
+                    console.log('User Details Match Found');
+                }
+                
+                else {
+                    $("#loginForm").addClass("fail");
+                    $("#loginForm").removeClass("success");
+
+                    $("#errormsg").removeClass("hideMessage");
+                    $("#errormsg").addClass("showMessage");
+                    console.log('User Details Match NOT Found');
+                }
+
+            $("#postResultDiv").html("<p>" + "Post success! <br>" + "Email address: " + user.userEmail + "</br>" + "Password: " + user.userPass + "</br>" + "Valid user: " + user.valid + "</p>");
+
+            },  
+            error: function(e){
+                // alert("Error!");
+                console.log("ERROR: ", e);
+            }
+        });
+
+      resetData();
+
+    } // ajaxPost()
+
+    function resetData(){
+        $("#userEmail").val("");
+        $("#userPass").val("");
+    } 
+    
+});
